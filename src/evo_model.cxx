cfb5162947902140ede637f9f1b8839baf04ef21
Fabian Klötzl <fabian@kloetzl.info>
2018-09-06 11:55:32 +0200

    speedup substitution counting

--- src/evo_model.cxx
+++ src/evo_model.cxx
@@ -5,6 +5,158 @@
 #include <iostream>
 #include <iterator>
 #include <numeric>
+#include <cstring>
+
+constexpr bool is_complement(char c, char d);
+
+#ifdef __AVX512BW__
+#ifdef __AVX512VL__
+#include <immintrin.h>
+
+typedef __m256i vec_type;
+
+size_t
+count_subst_avx512(
+	const char *self,
+	const char *other,
+	size_t length)
+{
+	size_t substitutions = 0;
+
+	const size_t vec_bytes = sizeof(vec_type);
+	size_t vec_offset = 0;
+	size_t vec_length = length / vec_bytes;
+
+	size_t equal = 0;
+	for (; vec_offset < vec_length; vec_offset++) {
+		vec_type self_chunk;
+		vec_type other_chunk;
+		memcpy(&self_chunk, self + vec_offset * vec_bytes, vec_bytes);
+		memcpy(&other_chunk, other + vec_offset * vec_bytes, vec_bytes);
+
+		unsigned int vmask = _mm256_cmpeq_epi8_mask(self_chunk, other_chunk);
+		equal += __builtin_popcount(vmask);
+	}
+
+	substitutions = vec_length * vec_bytes - equal;
+
+	size_t offset = vec_offset * vec_bytes;
+	for (; offset < length; offset++) {
+		if (self[offset] != other[offset]) {
+			substitutions++;
+		}
+	}
+
+	return substitutions;
+}
+
+#endif
+#endif
+
+#ifdef __AVX2__
+#include <immintrin.h>
+
+typedef __m256i vec_type;
+
+size_t
+count_subst_avx2(
+	const char *self,
+	const char *other,
+	size_t length)
+{
+	size_t substitutions = 0;
+	size_t offset = 0;
+
+	const size_t vec_bytes = sizeof(vec_type);
+	size_t vec_offset = 0;
+	size_t vec_length = (length / vec_bytes) & ~(size_t)1; // round down
+
+	size_t equal = 0;
+	for (; vec_offset < vec_length; vec_offset++) {
+		vec_type self_chunk;
+		vec_type other_chunk;
+		memcpy(&self_chunk, self + vec_offset * vec_bytes, vec_bytes);
+		memcpy(&other_chunk, other + vec_offset * vec_bytes, vec_bytes);
+
+		vec_type comp = _mm256_cmpeq_epi8(self_chunk, other_chunk);
+
+		unsigned int vmask = _mm256_movemask_epi8(comp);
+		equal += __builtin_popcount(vmask);
+
+		vec_offset++;
+		// second pass
+		memcpy(&self_chunk, self + vec_offset * vec_bytes, vec_bytes);
+		memcpy(&other_chunk, other + vec_offset * vec_bytes, vec_bytes);
+
+		comp = _mm256_cmpeq_epi8(self_chunk, other_chunk);
+
+		vmask = _mm256_movemask_epi8(comp);
+		equal += __builtin_popcount(vmask);
+	}
+
+	substitutions = vec_offset * vec_bytes - equal;
+
+	offset += vec_offset * vec_bytes;
+
+	for (; offset < length; offset++) {
+		if (self[offset] != other[offset]) {
+			substitutions++;
+		}
+	}
+
+	return substitutions;
+}
+
+#endif
+
+
+#ifdef __SSE2__
+#include <emmintrin.h>
+
+size_t
+intr(const char *self, const char *other, size_t length)
+{
+	size_t substitutions = 0;
+	size_t offset = 0;
+
+	size_t vec_offset = 0;
+	size_t vec_length = length / sizeof(__m128i);
+
+	substitutions += sizeof(__m128i) * vec_length;
+	for (; vec_offset < vec_length; vec_offset++) {
+		__m128i b;
+		memcpy(&b, self + vec_offset * sizeof(__m128i), sizeof(b));
+		__m128i o;
+		size_t pos = length - (vec_offset + 1) * sizeof(__m128i);
+		memcpy(&o, other + pos, sizeof(o));
+
+		__m128i mask = _mm_set_epi8(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+		__m128i reversed = _mm_shuffle_epi8(o, mask);
+
+		__m128i v1 = _mm_xor_si128(b, reversed);
+		__m128i mask6 = _mm_set1_epi8(6);
+		__m128i v2 = _mm_and_si128(v1, mask6);
+		__m128i mask4 = _mm_set1_epi8(4);
+		__m128i v3 = _mm_cmpeq_epi8(v2, mask4);
+
+		unsigned int vmask = _mm_movemask_epi8(v3);
+		// substitutions += sizeof(__m128i) - __builtin_popcount(vmask);
+		substitutions -= __builtin_popcount(vmask);
+	}
+
+	offset += vec_offset * sizeof(__m128i);
+
+	for (; offset < length; offset++) {
+		if (!is_complement(self[offset], other[length - 1 - offset])) {
+			substitutions++;
+		}
+	}
+
+	return substitutions;
+}
+
+#endif
+
 
 gsl_rng *RNG;
 
@@ -45,8 +197,27 @@ void evo_model::account(char a, char b) noexcept
 void evo_model::account(const char *sa, const char *sb, size_t length) noexcept
 {
 	size_t mutations = 0;
-	for (size_t k = 0; k < length; k++) {
-		if (UNLIKELY(sa[k] != sb[k])) {
+	size_t offset = 0;
+
+
+#ifdef __AVX512BW__
+#ifdef __AVX512VL__
+
+	mutations = count_subst_avx512(sa, sb, length);
+	offset += length;
+
+#endif
+#endif
+
+#ifdef __AVX2__
+
+	mutations += count_subst_avx2(sa + offset, sb + offset, length - offset);
+	offset += length;
+
+#endif
+
+	for (; offset < length; offset++) {
+		if (UNLIKELY(sa[offset] != sb[offset])) {
 			mutations++;
 		}
 	}
@@ -76,8 +247,15 @@ void evo_model::account_rev(const char *sa, const char *sb, size_t b_offset,
 							size_t length) noexcept
 {
 	size_t mutations = 0;
-	for (size_t k = 0; k < length; k++) {
-		if (UNLIKELY(!is_complement(sa[k], sb[b_offset - 1 - k]))) {
+	size_t offset = 0;
+
+#ifdef __SSE2__
+	mutations += intr(sa, sb + b_offset - length, length);
+	offset += length;
+#endif
+
+	for (; offset < length; offset++) {
+		if (UNLIKELY(!is_complement(sa[offset], sb[b_offset - 1 - offset]))) {
 			mutations++;
 		}
 	}
