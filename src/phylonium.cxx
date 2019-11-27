ef28e92d6ca623ec8c6519f066156b15f6fee61f
Fabian Klötzl <fabian@kloetzl.info>
2019-11-27 16:13:35 +0100

    limit io parallelization by -t

--- src/phylonium.cxx
+++ src/phylonium.cxx
@@ -232,7 +232,7 @@ int main(int argc, char *argv[])
 	auto queries = std::vector<sequence>(file_names.size());
 
 	// read all genomes
-	#pragma omp parallel for
+	#pragma omp parallel for num_threads(THREADS)
 	for (size_t i = 0; i < file_names.size(); i++) {
 		queries[i] = join(read_genome(file_names[i]));
 	}
